c61904221bef6ea3dab5949faf6cfd9fc9ee0a2f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-05-13 23:32:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,16 @@ bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="5998058674:AAFYAR8onsITeBo04JoUAaeszei5WhYGkyo"
+    bot_token="5782497998:AAHgzQGQ2Hehw0zUUoeld59SJXFZb2vVroY"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
     bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
-    
+@bot.on_message(filters.command('clear') & filters.private)
+def command1(bot,message): 
+    bot.send_message(message.chat.id, " تم الإخلاء",disable_web_page_preview=True)
+    subprocess.call(['unlink','transcription.txt'])      
+
 @bot.on_message(filters.private & filters.incoming & filters.audio )
 def _telegram_file(client, message):
   try: 
